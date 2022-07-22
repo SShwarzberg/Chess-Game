a693f3a554d30c10512d09e2552a13daf0b4bed0
Shalom Shwarzberg <sholomshwarzberg@gmail.com>
2022-07-22 16:35:59 -0400

    pieces blocking king from check can only move if moving won't cause check

--- src/InitialPosition.js
+++ src/InitialPosition.js
@@ -7,19 +7,19 @@ import horse from './images/horse.png'
 
 // player one initial positions
 const pawnsPlayerOne = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8']
-const bishopsPlayerOne = ['h3', 'c5']
-const rooksPlayerOne = ['c1', 'h8']
+const bishopsPlayerOne = ['h3', 'h6']
+const rooksPlayerOne = ['h1', 'h8']
 const horsesPlayerOne = ['h2', 'h7']
-const queenPlayerOne = ['f4']
+const queenPlayerOne = ['h5']
 const kingPlayerOne = ['h4']
 
 // player two initial position
-const pawnsPlayerTwo = ['b1', 'b2', 'b3', 'd4', 'b5', 'b6', 'b7', 'b8']
+const pawnsPlayerTwo = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8']
 const bishopsPlayerTwo = ['a3', 'a6']
 const rooksPlayerTwo = ['a1', 'a8']
 const horsesPlayerTwo = ['a2', 'a7']
-const queenPlayerTwo = ['c3']
-const kingPlayerTwo = ['c4']
+const queenPlayerTwo = ['a5']
+const kingPlayerTwo = ['a4']
 
 // player one piece info
 const piecesPlayerOne = [
